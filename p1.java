96a2f81dbcada49c33d44126b7fbb91c0dd694fd
PatelPratik8 <pratik7601@gmail.com>
2017-05-08 18:14:39 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,5 +1,15 @@
 import java.util.*;
 public static void main(String[] a)
 {
+  int i=0;
   System.out.println("hi my name is pratik");
+  if(i==0)
+  {
+    System.out.print("ok");
+    i=1;
+  }
+  else
+  {
+    System.out.prin("not ok");
+  }
 }
